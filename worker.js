7f93798371757de7318d401537912ee259036054
tavinpz <contatogustavordc@gmail.com>
2026-01-06 10:37:19 -0300

    Delete worker.js

--- worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request) {
-    const html = `COLE_TODO_O_CONTEUDO_DO_INDEX_HTML_AQUI`;
-    
-    return new Response(html, {
-      headers: { 'Content-Type': 'text/html;charset=UTF-8' }
-    });
-  }
-}
